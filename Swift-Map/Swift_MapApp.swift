3b8cc7b8d056717fc3edc1060befb8aae73307fb
IsaacExolivil <rodchinlopez@gmail.com>
2021-12-15 23:49:45 -0600

    Initial Commit

---
+++ Swift-Map/Swift_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_MapApp.swift
+//  Swift-Map
+//
+//  Created by Isaac Loez on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
